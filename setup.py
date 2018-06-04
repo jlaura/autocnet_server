26f66dfde1b807e652aff412149c64c48514f517
jay <jlaura@asu.edu>
2018-06-04 13:03:43 -0700

    travis!

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 setup(
     name='autocnet_server',
-    version='0.0.1',
+    version='0.1.0',
     long_description='',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    package_requires=[],
     scripts=['bin/acn_create_network', 'bin/acn_estimate_fundamental',
              'bin/acn_extract_features', 'bin/acn_generate_mosaic',
              'bin/acn_ring_match']
