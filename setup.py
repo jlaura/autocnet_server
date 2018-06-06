6d87eeed03a4fe4b159b2ebb0d8f34b0345c56a2
jay <jlaura@asu.edu>
2018-06-05 20:57:55 -0700

    Fixes setup for bin renaming

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=['bin/acn_create_network', 'bin/acn_estimate_fundamental',
+    scripts=['bin/acn_create_network', 'bin/acn_compute_fundamental_matrix',
              'bin/acn_extract_features', 'bin/acn_generate_mosaic',
              'bin/acn_ring_match']
 )
